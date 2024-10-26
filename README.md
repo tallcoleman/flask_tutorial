c97bf583ec9c1cad57a6709eb04ec24e9f94d569
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-10-26 11:58:30 -0400

    Chapter 1 of tutorial
    project set-up

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # flask_tutorial
  
+## How to run
+
+```bash
+$ uv run flask run
+```
--- src/flask_tutorial/__init__.py
+++ src/flask_tutorial/__init__.py
@@ -1,2 +1,5 @@
-def hello() -> str:
-    return "Hello from flask-tutorial!"
+from flask import Flask
+
+app = Flask(__name__)
+
+from flask_tutorial import routes  # noqa

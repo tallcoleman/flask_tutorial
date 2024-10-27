fc0b91353c8272ac86d526afd152cafbb2db3af8
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-10-26 21:16:18 -0400

    Chapter 4

--- src/flask_tutorial/__init__.py
+++ src/flask_tutorial/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from flask_tutorial import routes  # noqa
+from flask_tutorial import routes, models  # noqa E402
--- src/microblog.py
+++ src/microblog.py
@@ -1,1 +1,15 @@
-from flask_tutorial import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from flask_tutorial import app, db
+from flask_tutorial.models import User, Post
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "sa": sa,
+        "so": so,
+        "db": db,
+        "User": User,
+        "Post": Post,
+    }

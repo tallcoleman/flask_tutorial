fb8c29ac95579159776ce1ac01df497ebb2c1bac
Ben Coleman <ben.coleman@alum.utoronto.ca>
2024-12-06 22:25:04 -0500

    Chapter 10

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ $ uv run task dev
 $ uv run flask db migrate -m "DESCRIPTION HERE"
 $ uv run flask db upgrade
 ```
+
+## Steps to building a feature
+
+- need to update template file(s)
+- need to update view function
+- may need to add a form class
+- may need to update the database schema
--- src/flask_tutorial/models.py
+++ src/flask_tutorial/models.py
@@ -1,13 +1,15 @@
 from datetime import datetime, timezone
+from time import time
 from hashlib import md5
 from typing import Optional
 
+import jwt
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flask_tutorial import db, login
+from flask_tutorial import db, login, app
 
 
 followers = sa.Table(
@@ -103,6 +105,23 @@ class User(UserMixin, db.Model):
             .group_by(Post)
             .order_by(Post.timestamp.desc())
         )
+
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {"reset_password": self.id, "exp": time() + expires_in},
+            app.config["SECRET_KEY"],
+            algorithm="HS256",
+        )
+
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config["SECRET_KEY"], algorithms=["HS256"])[
+                "reset_password"
+            ]
+        except:
+            return
+        return db.session.get(User, id)
 
 
 class Post(db.Model):
